b80700748e123b50b292738879a745c1eea0c5d9
Dorrthrax <dorrthrax@gmail.com>
2013-12-03 19:36:36 -0500

    Frist Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pangasius::Application.config.secret_key_base = 'ee9995345755580afc61f0a1d51ff453825b7274e9b8ea47f00e4c226ca32e994a8c7157bc8282d9e67b23a2394352ccecc8d902dbcfb9ab4e83dd539ad8dbd8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pangasius::Application.config.session_store :cookie_store, key: '_pangasius_session'
